66914263a9e1c7039507f94b64a66cacc27d52b6
atsushi <green515tea@gmail.com>
2023-10-04 00:37:10 +0900

    利用例をシンプル化

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import time
-from test.test_util import generate_big_graph
-
 import networkx as nx
 
 import networkc as nc
@@ -14,18 +11,15 @@ def main():
     G.add_edge("B", "C", weight=2)
     G.add_edge("C", "D", weight=1)
     G.add_edge("D", "A", weight=1)
-    G = generate_big_graph(num_nodes=1000)
 
-    start = time.time()
     res = nc.floyd_warshall(G, weight="weight")
-    print("C言語実装のfloyd_warshall", time.time() - start)
     print(res["A"]["D"])
 
-    start = time.time()
     res_orig = nx.floyd_warshall(G, weight="weight")
-    print("NetworkX実装のfloyd_warshall", time.time() - start)
     print(res_orig["A"]["D"])
+
     print(res == res_orig)
+    nx.all_pairs_dijkstra_path
 
 
 if __name__ == "__main__":
